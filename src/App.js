9344b44287223cc74d2141f112b92d3dacfb2b0e
Neeraja <67228491+neerajakulkarni@users.noreply.github.com>
2024-05-25 17:44:45 -0700

    Populating map with pins, user location

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
+import ReactDOM from "react-dom";
 import SearchBar from './components/SearchBar';
+import geoJson from "./chicago-parks.json";
+import "./index.css";
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYXVkLWRyZWFtcyIsImEiOiJjbHdtazk1eTkwaDUxMmlwb2d1ZzM1N3ZtIn0.fK_tYF0yFBfCum4y4LXtSA';
+
+const Marker = ({ onClick, children, feature }) => {
+  const _onClick = () => {
+    onClick(feature.properties.description);
+  };
+
+  return (
+    <button onClick={_onClick} className="marker">
+      {children}
+    </button>
+  );
+};
 
 export default function App() {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(-118.2426);
-  const [lat, setLat] = useState(34.0549);
+  const [lng, setLng] = useState(-117.838914978);
+  const [lat, setLat] = useState(33.6405407712);
   const [zoom, setZoom] = useState(14);
   const [state, setState] = useState({dataSource: []});
 
@@ -26,7 +41,41 @@ export default function App() {
       setLat(map.current.getCenter().lat.toFixed(4));
       setZoom(map.current.getZoom().toFixed(2));
     });
+
+    // Add geolocate control to the map
+    map.current.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true
+        },
+        trackUserLocation: true,
+        // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        showUserHeading: true
+      })
+    );
+
+    // Add navigation control (the +/- zoom buttons)
+    map.current.addControl(new mapboxgl.NavigationControl());
+
+    if (geoJson && geoJson.features) {
+      geoJson.features.forEach((feature) => {
+      const ref = React.createRef();
+      ref.current = document.createElement("div");
+      ReactDOM.render(
+        <Marker onClick={markerClicked} feature={feature} />,
+        ref.current
+      );
+      new mapboxgl.Marker(ref.current).setLngLat(feature.geometry.coordinates).addTo(map.current);
+      });
+    }
+
+    
   });
+
+  const markerClicked = (title) => {
+    window.alert(title);
+  };
+
 
   return (
     <div>
