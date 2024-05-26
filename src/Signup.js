d8828e246501b16a48c0c8042b778bbe7d65dcbc
Sharon <sharon.m0626@gmail.com>
2024-05-26 08:25:10 -0700

    Add signup page

--- src/Signup.js
+++ src/Signup.js
@@ -1,75 +1,246 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <div className="sign-up-form">
-      <h2>Sign up</h2>
-      <form>
-        <div className="form-group">
-          <label htmlFor="name">Name *</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            placeholder="Name"
-            required
-          />
-        </div>
+    <>
+      {/*
+        This example requires updating your template:
 
-        <div className="form-group">
-          <label htmlFor="email">Email *</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Email"
-            required
-          />
-        </div>
+        ```
+        <html class="h-full bg-white">
+        <body class="h-full">
+        ```
+      */}
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
 
-        <div className="form-group">
-          <label htmlFor="password">Password *</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            placeholder="Password"
-            required
-          />
-        </div>
+            <fieldset>
+              <legend className="text-sm font-semibold leading-6 text-gray-900">
+                Gender
+              </legend>
+              <div className="mt-6 space-y-6">
+                <div className="flex items-center gap-x-3">
+                  <input
+                    id="push-everything-gender"
+                    name="push-notifications-gender"
+                    type="radio"
+                    className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                  />
+                  <label
+                    htmlFor="push-everything-gender"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    female
+                  </label>
+                </div>
+                <div className="flex items-center gap-x-3">
+                  <input
+                    id="push-email-gender"
+                    name="push-notifications-gender"
+                    type="radio"
+                    className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                  />
+                  <label
+                    htmlFor="push-email-gender"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    male
+                  </label>
+                </div>
+                <div className="flex items-center gap-x-3">
+                  <input
+                    id="push-nothing-gender"
+                    name="push-notifications-gender"
+                    type="radio"
+                    className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                  />
+                  <label
+                    htmlFor="push-nothing-gender"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    other
+                  </label>
+                </div>
+              </div>
+            </fieldset>
 
-        <div className="form-group">
-          <label>Gender</label>
-          <div className="gender-options">
-            <input type="radio" id="female" name="gender" value="female" />
-            <label htmlFor="female">Female</label>
-            <input type="radio" id="male" name="gender" value="male" />
-            <label htmlFor="male">Male</label>
-            <input type="radio" id="other" name="gender" value="other" />
-            <label htmlFor="other">Other</label>
-          </div>
-        </div>
+            <fieldset>
+              <legend className="text-sm font-semibold leading-6 text-gray-900">
+                Descent
+              </legend>
+              <div className="mt-6 space-y-6">
+                <div className="flex items-center gap-x-3">
+                  <input
+                    id="push-everything-descent"
+                    name="push-notifications-descent"
+                    type="radio"
+                    className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                  />
+                  <label
+                    htmlFor="push-everything-descent"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Asian
+                  </label>
+                </div>
+                <div className="flex items-center gap-x-3">
+                  <input
+                    id="push-everything-descent"
+                    name="push-notifications-descent"
+                    type="radio"
+                    className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                  />
+                  <label
+                    htmlFor="push-everything-descent"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    asian
+                  </label>
+                </div>
+                <div className="flex items-center gap-x-3">
+                  <input
+                    id="push-everything-descent"
+                    name="push-notifications-descent"
+                    type="radio"
+                    className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                  />
+                  <label
+                    htmlFor="push-everything-descent"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    black
+                  </label>
+                </div>
+                <div className="flex items-center gap-x-3">
+                  <input
+                    id="push-everything-descent"
+                    name="push-notifications-descent"
+                    type="radio"
+                    className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                  />
+                  <label
+                    htmlFor="push-everything-descent"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    hispanic
+                  </label>
+                </div>
+                <div className="flex items-center gap-x-3">
+                  <input
+                    id="push-everything-descent"
+                    name="push-notifications-descent"
+                    type="radio"
+                    className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                  />
+                  <label
+                    htmlFor="push-everything-descent"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    white
+                  </label>
+                </div>
+                <div className="flex items-center gap-x-3">
+                  <input
+                    id="push-everything-descent"
+                    name="push-notifications-descent"
+                    type="radio"
+                    className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                  />
+                  <label
+                    htmlFor="push-everything-descent"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    pacific
+                  </label>
+                  <div className="flex items-center gap-x-3">
+                    <input
+                      id="push-everything-descent"
+                      name="push-notifications-descent"
+                      type="radio"
+                      className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                    />
+                    <label
+                      htmlFor="push-everything-descent"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      indian
+                    </label>
+                    <div className="flex items-center gap-x-3">
+                      <input
+                        id="push-everything-descent"
+                        name="push-notifications-descent"
+                        type="radio"
+                        className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                      />
+                      <label
+                        htmlFor="push-everything-descent"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
+                        other
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </fieldset>
 
-        <div className="form-group">
-          <label htmlFor="age">Age</label>
-          <input type="number" id="age" name="age" placeholder="Age" />
-        </div>
+            <div>
+              <label
+                htmlFor="age"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                age
+              </label>
+              <div className="mt-2">
+                <input
+                  id="age"
+                  name="age"
+                  type="age"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
 
-        <div className="form-group">
-          <label htmlFor="ethnicity">Ethnicity</label>
-          <div id="ethnicity" name="ethnicity">
-            <input type="radio" id="female" name="gender" value="female" />
-            <label htmlFor="female">Female</label>
-            <input type="radio" id="male" name="gender" value="male" />
-            <label htmlFor="male">Male</label>
-            <input type="radio" id="other" name="gender" value="other" />
-            <label htmlFor="other">Other</label>
-          </div>
+            <div>
+              <Link to="/">
+                <button className="flex w-full justify-center rounded-md bg-gray-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500">
+                  Cancel
+                </button>
+              </Link>
+              <Link to="/">
+                <button
+                  type="submit"
+                  className="mt-2 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Save
+                </button>
+              </Link>
+            </div>
+          </form>
         </div>
-
-        <button type="submit">Sign up</button>
-      </form>
-    </div>
+      </div>
+    </>
   );
 };
 
